39bba5dd9ccf75874bf3b8e47fbae6b6eb2eb9a1
tharun <tharun.damera4@gmail.com>
2025-10-17 16:50:22 +0530

    Fix the serde error in uuid and chrono by adding those features

--- src/models.rs
+++ src/models.rs
@@ -1,22 +1,22 @@
-use chrono::Utc;
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Note {
-    id: String,
+    id: Uuid,
     title: String,
     content: String,
-    created_at: String,
+    created_at: DateTime<Utc>,
 }
 
 impl Note {
     pub fn new(title: String, content: String) -> Self {
         Note {
-            id: Uuid::new_v4().to_string(),
+            id: Uuid::new_v4(),
             title,
             content,
-            created_at: Utc::now().to_rfc3339(),
+            created_at: Utc::now(),
         }
     }
 }
