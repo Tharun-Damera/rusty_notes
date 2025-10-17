b0bb997cd333c11d03f6aa7f88ec074d9dd4e0d9
tharun <tharun.damera4@gmail.com>
2025-10-17 19:48:48 +0530

    Add anyhow crate for easy error handling

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,14 @@ mod routes;
 use crate::routes::{create_note, get_note};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/", get(|| async { "Hello World!" }))
         .route("/notes", post(create_note))
         .route("/notes/{note_id}", get(get_note));
 
-    let listener = tokio::net::TcpListener::bind("localhost:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("localhost:3000").await?;
 
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await?;
+    Ok(())
 }
